46635fc9e685fd68736077f2d0dfa464e4b60cc7
MosNes <7hekarl@gmail.com>
2022-12-13 17:20:04 -0500

    created initial placeholder view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <Character />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import Character from '@/views/Character.vue'
+
+  
 </script>
